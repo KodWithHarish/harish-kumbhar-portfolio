5bd25cf36a629783bbc52cd3d2b8a9c5308d3da1
KodWithHarish <harishkumbhar938@gmail.com>
2024-07-27 12:55:45 +0530

    Created Required Pages

---
+++ src/templates/MainSection.js
@@ -0,0 +1,9 @@
+import React from "react";
+function MainSection() {
+  return (
+    <>
+      <h1>MainSection</h1>
+    </>
+  );
+}
+export default MainSection;
---
+++ src/templates/TopBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+function TopBar() {
+  return (
+    <>
+      <h1>TopBar</h1>
+    </>
+  );
+}
+export default TopBar;
